eeece59432f7665e62416f3d92efb67a05bfd248
BlazeOne <yann.tsatsaris@gmail.com>
2025-04-14 14:34:20 +0200

    Ajout de la fonctionnalité pour afficher dynamiquement les images des personnages dans la page characters.html

--- routes/RouteCharacters.py
+++ routes/RouteCharacters.py
@@ -1,6 +1,24 @@
+import os
 from flask import Flask, render_template
 
 def characters(app: Flask):
     @app.route('/characters')
     def inner_characters():
-        return render_template('characters.html')
+        # Chemin vers le dossier "personnages"
+        base_path = os.path.join(app.static_folder, 'images', 'personnages')
+        images = []
+
+        # Parcourir les sous-dossiers pour trouver les images
+        for type_folder in os.listdir(base_path):
+            type_path = os.path.join(base_path, type_folder)
+            if os.path.isdir(type_path):
+                for character_folder in os.listdir(type_path):
+                    character_path = os.path.join(type_path, character_folder)
+                    if os.path.isdir(character_path):
+                        for file in os.listdir(character_path):
+                            if file.endswith('_Codex.png'):
+                                # Ajouter le chemin relatif de l'image
+                                image_path = f'images/personnages/{type_folder}/{character_folder}/{file}'
+                                images.append(image_path)
+
+        return render_template('characters.html', images=images)
