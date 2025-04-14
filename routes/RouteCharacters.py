658705d2a2596856b290d9e3615d8eea26f9f24c
BlazeOne <yann.tsatsaris@gmail.com>
2025-04-14 15:24:41 +0200

    Mise à jour de la logique de récupération des images des personnages pour correspondre au format de nommage et ajout de l'image dans les détails du personnage.

--- routes/RouteCharacters.py
+++ routes/RouteCharacters.py
@@ -16,9 +16,8 @@ def characters(app: Flask):
                     character_path = os.path.join(type_path, character_folder)
                     if os.path.isdir(character_path):
                         for file in os.listdir(character_path):
-                            # Vérifier que le fichier se termine par "_Codex.png" mais pas "_Weapon_Codex.png" ou "_Arme_Codex.png"
+                            # Vérifier que le fichier correspond au format "Type_NomPersonnage_Codex.png"
                             if file.endswith('_Codex.png') and not ('_Weapon_Codex.png' in file or '_Arme_Codex.png' in file):
-                                # Ajouter le chemin relatif de l'image et le nom du personnage
                                 character_name = character_folder.split('_')[-1]  # Extraire le nom du personnage
                                 images.append({
                                     'path': f'images/Personnages/{type_folder}/{character_folder}/{file}',
@@ -29,10 +28,28 @@ def characters(app: Flask):
 
     @app.route('/characters/<name>')
     def character_details(name):
+        # Chemin vers le dossier "personnages"
+        base_path = os.path.join(app.static_folder, 'images', 'Personnages')
+        character_image = None
+
+        # Parcourir les sous-dossiers pour trouver l'image "Type_NomPersonnage.png"
+        for type_folder in os.listdir(base_path):
+            type_path = os.path.join(base_path, type_folder)
+            if os.path.isdir(type_path):
+                for character_folder in os.listdir(type_path):
+                    if name in character_folder:  # Vérifie si le nom correspond au dossier
+                        character_path = os.path.join(type_path, character_folder)
+                        if os.path.isdir(character_path):
+                            for file in os.listdir(character_path):
+                                # Vérifier que le fichier correspond au format "Type_NomPersonnage.png"
+                                if file == f'{character_folder}.png':
+                                    character_image = f'images/Personnages/{type_folder}/{character_folder}/{file}'
+                                    break
+
         # Exemple de données fictives pour un personnage
         character_info = {
             'name': name,
             'description': f'{name} is a powerful character in Solo Leveling Arise.',
-            'image': f'images/Personnages/SLA_Personnages_Type1/SSR_Type1_{name}/Type1_{name}_Codex.png'
+            'image': character_image
         }
         return render_template('character_details.html', character=character_info)
