72cc86a9e9d405faa2d9465c7d70454081662d62
BlazeOne <yann.tsatsaris@gmail.com>
2025-04-16 14:16:43 +0200

    Ajout d'une image de fond pour les personnages dans les détails, avec gestion de l'absence d'image.

--- routes/RouteCharacters.py
+++ routes/RouteCharacters.py
@@ -42,6 +42,12 @@ def characters(app: Flask):
         character_info['image'] = image_path
         character_info['description'] = f"{character_info['name']} is a powerful character of type {character_info['type']} in Solo Leveling Arise."
 
+        # Ajouter l'image de fond si la clé 'fond' existe
+        if 'fond' in character_info:
+            character_info['background_image'] = f'images/Personnages/{type_folder}/{character_folder}/{character_info["fond"]}'
+        else:
+            character_info['background_image'] = None  # Pas d'image de fond
+
         # Construire les chemins des images des passifs
         for passive in character_info.get('passives', []):
             if 'image' in passive:
