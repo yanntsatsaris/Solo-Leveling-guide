3818df34027059e046bd317625c411fd2077a396
BlazeOne <yann.tsatsaris@gmail.com>
2025-04-30 13:19:57 +0200

    Ajoute la redirection de stdout et stderr vers Gunicorn avec configuration de logging

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
+import sys
+import logging
 from flask import Flask
 from routes.RouteHome import home
 from routes.RouteGame_contents import game_contents
 from routes.RouteCharacters import characters
 from routes.RouteGuides import guides
 from routes.RouteSJW import SJW
+
+# Rediriger stdout et stderr vers Gunicorn
+logging.basicConfig(stream=sys.stdout, level=logging.DEBUG)
+sys.stdout = sys.stderr
 
 app = Flask(__name__, static_folder='static')
 
