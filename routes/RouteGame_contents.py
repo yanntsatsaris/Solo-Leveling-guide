b1b1c0735a9add2a3052fdf66c5c25c2db34db45
BlazeOne <yann.tsatsaris@gmail.com>
2025-05-12 12:49:48 +0200

    Ajoute les routes et le modèle HTML pour les modes de jeu, ainsi que le fichier CSS associé pour le style.

--- routes/RouteGame_contents.py
+++ routes/RouteGame_contents.py
@@ -4,3 +4,7 @@ def game_contents(app: Flask):
     @app.route('/game-contents')
     def inner_game_contents():
         return render_template('game_contents.html')
+    
+    @app.route('/game-contents/game-modes')
+    def game_modes():
+        return render_template('game_modes.html')
