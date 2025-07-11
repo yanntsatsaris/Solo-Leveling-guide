e86ac735a761ec0b0fef7e92956fb3c57b5f503c
BlazeOne <yann.tsatsaris@gmail.com>
2025-07-11 15:10:43 +0200

    Aucune modification apportée au code

--- routes/RouteUsers.py
+++ routes/RouteUsers.py
@@ -2,14 +2,14 @@ from flask import request, jsonify, session
 from static.Controleurs.ControleurLdap import ControleurLdap
 from static.Controleurs.ControleurLog import write_log
 
-ldap_ctrl = ControleurLdap()
 
 def users(app):
     @app.route('/login', methods=['POST'])
     def login():
+        ldap = ControleurLdap()
         username = request.form.get('username')
         password = request.form.get('password')
-        if ldap_ctrl.authenticate_user(username, password):
+        if ldap.authenticate_user(username, password):
             session['user'] = username
             write_log(f"Connexion réussie", log_level="INFO", username=username)
             return jsonify({'success': True})
@@ -18,17 +18,18 @@ def users(app):
 
     @app.route('/register', methods=['POST'])
     def register():
+        ldap = ControleurLdap()
         username = request.form.get('new_username')
         email = request.form.get('new_email')
         password = request.form.get('new_password')
-        dn = f"uid={username},{ldap_ctrl.config.get_config('LDAP', 'base_dn')}"
+        dn = f"uid={username},{ldap.config.get_config('LDAP', 'base_dn')}"
         attrs = [
             ('objectClass', [b'inetOrgPerson']),
             ('uid', [username.encode('utf-8')]),
             ('mail', [email.encode('utf-8')]),
             ('userPassword', [password.encode('utf-8')])
         ]
-        if ldap_ctrl.add_entry(dn, attrs):
+        if ldap.add_entry(dn, attrs):
             write_log(f"Création de compte réussie ({email})", log_level="INFO", username=username)
             return jsonify({'success': True})
         write_log(f"Échec de création de compte ({email})", log_level="ERROR", username=username)
