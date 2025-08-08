211ef448c86e3ae5da7107a0c16a01c9c05ea7b9
BlazeOne <yann.tsatsaris@gmail.com>
2025-08-08 10:21:44 +0200

    Ajout de l'identifiant d'évolution dans les requêtes get_evolutions et get_evolutions_full, mise à jour de la méthode update_evolution pour inclure l'identifiant d'évolution, et modification de la méthode add_evolution pour insérer cet identifiant.

--- static/Controleurs/sql_entities/characters/evolutions_sql.py
+++ static/Controleurs/sql_entities/characters/evolutions_sql.py
@@ -7,7 +7,7 @@ class EvolutionsSql:
     def get_evolutions(self, char_id, language, type_folder, char_folder):
         write_log(f"Requête get_evolutions pour char_id={char_id}, langue={language}", log_level="DEBUG")
         self.cursor.execute("""
-            SELECT ce.character_evolutions_evolution_id, ce.character_evolutions_number, ce.character_evolutions_type, ce.character_evolutions_range, cet.character_evolution_translations_description
+            SELECT ce.evolution_id, ce.character_evolutions_evolution_id, ce.character_evolutions_number, ce.character_evolutions_type, ce.character_evolutions_range, cet.character_evolution_translations_description
             FROM character_evolutions ce
             LEFT JOIN character_evolution_translations cet ON cet.character_evolution_translations_character_evolutions_id = ce.character_evolutions_id
             WHERE ce.character_evolutions_characters_id = %s AND (cet.character_evolution_translations_language = %s OR cet.character_evolution_translations_language IS NULL)
@@ -15,17 +15,18 @@ class EvolutionsSql:
         return [
             {
                 'id': row[0],  # Ajoute l'id
-                'number': row[1],
-                'type': row[2],
-                'range': row[3],
-                'description': row[4] if row[4] else ''
+                'evolution_id': row[1],
+                'number': row[2],
+                'type': row[3],
+                'range': row[4],
+                'description': row[5] if row[5] else ''
             }
             for row in self.cursor.fetchall()
         ]
 
     def get_evolutions_full(self, char_id, language):
         self.cursor.execute("""
-            SELECT ce.character_evolutions_id, ce.character_evolutions_number, cet.character_evolution_translations_description
+            SELECT ce.character_evolutions_id, ce.character_evolutions_evolution_id, ce.character_evolutions_number, cet.character_evolution_translations_description
             FROM character_evolutions ce
             LEFT JOIN character_evolution_translations cet ON cet.character_evolution_translations_character_evolutions_id = ce.character_evolutions_id
             WHERE ce.character_evolutions_characters_id = %s AND (cet.character_evolution_translations_language = %s OR cet.character_evolution_translations_language IS NULL)
@@ -33,23 +34,24 @@ class EvolutionsSql:
         return [
             {
                 'id': row[0],
-                'number': row[1],
-                'description': row[2] if row[2] else ''
+                'evolution_id': row[1],
+                'number': row[2],
+                'description': row[3] if row[3] else ''
             }
             for row in self.cursor.fetchall()
         ]
 
-    def update_evolution(self, eid, char_id, evo_idx, desc, language):
+    def update_evolution(self, eid, evolution_id, char_id, evo_idx, desc, language):
         self.cursor.execute("""
-            UPDATE character_evolution_translations SET character_evolution_translations_description=%s
+            UPDATE character_evolution_translations SET character_evolution_translations_evolution_id=%s, character_evolution_translations_description=%s
             WHERE character_evolution_translations_character_evolutions_id=%s AND character_evolution_translations_language=%s
-        """, (desc, eid, language))
+        """, (evolution_id, desc, eid, language))
 
-    def add_evolution(self, char_id, evo_idx, desc, language):
+    def add_evolution(self, char_id, evo_idx, evolution_id, desc, language):
         self.cursor.execute("""
-            INSERT INTO character_evolutions (character_evolutions_characters_id, character_evolutions_number)
-            VALUES (%s, %s) RETURNING character_evolutions_id
-        """, (char_id, evo_idx))
+            INSERT INTO character_evolutions (character_evolutions_characters_id, character_evolutions_number, character_evolutions_evolution_id)
+            VALUES (%s, %s, %s) RETURNING character_evolutions_id
+        """, (char_id, evo_idx, evolution_id))
         eid = self.cursor.fetchone()[0]
         self.cursor.execute("""
             INSERT INTO character_evolution_translations (character_evolution_translations_character_evolutions_id, character_evolution_translations_language, character_evolution_translations_description)
