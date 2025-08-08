4a8dac6b60ca80444203a89eb1f811abcbbcac9a
BlazeOne <yann.tsatsaris@gmail.com>
2025-08-08 10:35:08 +0200

    Correction de la requête get_evolutions pour utiliser character_evolutions_id au lieu de character_evolution_id.

--- static/Controleurs/sql_entities/characters/evolutions_sql.py
+++ static/Controleurs/sql_entities/characters/evolutions_sql.py
@@ -7,7 +7,7 @@ class EvolutionsSql:
     def get_evolutions(self, char_id, language, type_folder, char_folder):
         write_log(f"Requête get_evolutions pour char_id={char_id}, langue={language}", log_level="DEBUG")
         self.cursor.execute("""
-            SELECT ce.character_evolution_id, ce.character_evolutions_evolution_id, ce.character_evolutions_number, ce.character_evolutions_type, ce.character_evolutions_range, cet.character_evolution_translations_description
+            SELECT ce.character_evolutions_id, ce.character_evolutions_evolution_id, ce.character_evolutions_number, ce.character_evolutions_type, ce.character_evolutions_range, cet.character_evolution_translations_description
             FROM character_evolutions ce
             LEFT JOIN character_evolution_translations cet ON cet.character_evolution_translations_character_evolutions_id = ce.character_evolutions_id
             WHERE ce.character_evolutions_characters_id = %s AND (cet.character_evolution_translations_language = %s OR cet.character_evolution_translations_language IS NULL)
