5c96a50681facd57b9ae168f7c16ce244c64ff3d
BlazeOne <yann.tsatsaris@gmail.com>
2025-09-08 15:43:20 +0200

    fix: corriger la récupération des statistiques des armes et ajuster la requête SQL pour les évolutions

--- static/Controleurs/sql_entities/sjw/sjw_weapons_sql.py
+++ static/Controleurs/sql_entities/sjw/sjw_weapons_sql.py
@@ -4,7 +4,7 @@ class SJWWeaponsSql:
 
     def get_weapons(self, sjw_id, language):
         self.cursor.execute("""
-            SELECT w.sjw_weapons_id, w.sjw_weapons_image, t.sjw_weapon_translations_name, t.sjw_weapon_translations_description
+            SELECT w.sjw_weapons_id, w.sjw_weapons_image, t.sjw_weapon_translations_name, t.sjw_weapon_translations_stats
             FROM sjw_weapons w
             JOIN sjw_weapon_translations t ON t.sjw_weapon_translations_sjw_weapons_id = w.sjw_weapons_id
             WHERE w.sjw_weapons_sjw_id = %s AND t.sjw_weapon_translations_language = %s
@@ -16,7 +16,7 @@ class SJWWeaponsSql:
                 'id': weapon_id,
                 'image': row[1],
                 'name': row[2],
-                'description': row[3],
+                'stats': row[3],
                 'evolutions': self.get_evolutions(weapon_id, language)
             }
             weapons.append(weapon)
@@ -24,17 +24,21 @@ class SJWWeaponsSql:
 
     def get_evolutions(self, weapon_id, language):
         self.cursor.execute("""
-            SELECT e.sjw_weapon_evolutions_id, e.sjw_weapon_evolutions_type, t.sjw_weapon_evolution_translations_name, t.sjw_weapon_evolution_translations_description
+            SELECT e.sjw_weapon_evolutions_id, e.sjw_weapon_evolutions_number, e.sjw_weapon_evolutions_range, e.sjw_weapon_evolutions_type,
+                   t.sjw_weapon_evolution_translations_description
             FROM sjw_weapon_evolutions e
-            JOIN sjw_weapon_evolution_translations t ON t.sjw_weapon_evolution_translations_evolution_id = e.sjw_weapon_evolutions_id
-            WHERE e.sjw_weapon_evolutions_sjw_weapons_id = %s AND t.sjw_weapon_evolution_translations_language = %s
-        """, (weapon_id, language))
+            LEFT JOIN sjw_weapon_evolution_translations t
+                ON t.sjw_weapon_evolution_translations_sjw_weapon_evolutions_id = e.sjw_weapon_evolutions_id
+                AND t.sjw_weapon_evolution_translations_language = %s
+            WHERE e.sjw_weapon_evolutions_sjw_weapons_id = %s
+        """, (language, weapon_id))
         return [
             {
                 'id': row[0],
-                'type': row[1],
-                'name': row[2],
-                'description': row[3]
+                'number': row[1],
+                'range': row[2],
+                'type': row[3],
+                'description': row[4]
             }
             for row in self.cursor.fetchall()
         ]
