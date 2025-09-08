afb9b8811d02a018a7eac25e91b3c3d05097dd4e
BlazeOne <yann.tsatsaris@gmail.com>
2025-09-08 15:39:44 +0200

    fix: corriger la méthode get_skills pour gérer le paramètre de langue par défaut et ajuster la requête SQL

--- static/Controleurs/sql_entities/sjw/sjw_skills_sql.py
+++ static/Controleurs/sql_entities/sjw/sjw_skills_sql.py
@@ -2,19 +2,19 @@ class SJWSkillsSql:
     def __init__(self, cursor):
         self.cursor = cursor
 
-    def get_skills(self, sjw_id, language):
+    def get_skills(self, sjw_id, language=None):
         self.cursor.execute("""
-            SELECT s.sjw_skills_id, s.sjw_skills_image, t.sjw_skill_translations_name, t.sjw_skill_translations_description
-            FROM sjw_skills s
-            JOIN sjw_skill_translations t ON t.sjw_skill_translations_sjw_skills_id = s.sjw_skills_id
-            WHERE s.sjw_skills_sjw_id = %s AND t.sjw_skill_translations_language = %s
-        """, (sjw_id, language))
+            SELECT sjw_skills_id, sjw_skills_name, sjw_skills_type, sjw_skills_description, sjw_skills_image
+            FROM sjw_skills
+            WHERE sjw_skills_sjw_id = %s
+        """, (sjw_id,))
         return [
             {
                 'id': row[0],
-                'image': row[1],
-                'name': row[2],
-                'description': row[3]
+                'name': row[1],
+                'type': row[2],
+                'description': row[3],
+                'image': row[4]
             }
             for row in self.cursor.fetchall()
         ]
