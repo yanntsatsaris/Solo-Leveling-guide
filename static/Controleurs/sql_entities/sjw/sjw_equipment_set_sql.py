eb3abcbf96e22457a7573d9ddce7d6c5f4993f3f
BlazeOne <yann.tsatsaris@gmail.com>
2025-09-09 15:34:04 +0200

    fix: améliorer la récupération des ensembles d'équipement avec des descriptions et des statistiques de focus

--- static/Controleurs/sql_entities/sjw/sjw_equipment_set_sql.py
+++ static/Controleurs/sql_entities/sjw/sjw_equipment_set_sql.py
@@ -12,20 +12,54 @@ class SJWEquipmentSetSql:
         sets = []
         for row in self.cursor.fetchall():
             set_id, set_name = row
+            # Description traduite du set
+            self.cursor.execute("""
+                SELECT sjw_equipment_set_translations_description
+                FROM sjw_equipment_set_translations
+                WHERE sjw_equipment_set_translations_equipment_sets_id = %s AND sjw_equipment_set_translations_language = %s
+            """, (set_id, language))
+            desc_row = self.cursor.fetchone()
+            set_description = desc_row[0] if desc_row else ""
+            # Focus stats
+            self.cursor.execute("""
+                SELECT sjw_equipment_focus_stats_name
+                FROM sjw_equipment_focus_stats
+                WHERE sjw_equipment_focus_stats_sjw_equipment_sets_id = %s
+            """, (set_id,))
+            focus_stats = [fs_row[0] for fs_row in self.cursor.fetchall()]
             # Artefacts
             self.cursor.execute("""
-                SELECT a.sjw_artefacts_id, a.sjw_artefacts_number, a.sjw_artefacts_image, a.sjw_artefacts_main_stat
+                SELECT a.sjw_artefacts_id, a.sjw_artefacts_image, a.sjw_artefacts_main_stat, a.sjw_artefacts_number, a.sjw_artefacts_set
                 FROM sjw_artefacts a
                 WHERE a.sjw_artefacts_sjw_equipment_sets_id = %s
                 ORDER BY a.sjw_artefacts_number
             """, (set_id,))
             artefacts = []
             for a_row in self.cursor.fetchall():
+                artefact_id, artefact_image, artefact_main_stat, artefact_number, artefact_set = a_row
+                # Traduction
+                self.cursor.execute("""
+                    SELECT sjw_artefact_translations_language, sjw_artefact_translations_name
+                    FROM sjw_artefact_translations
+                    WHERE sjw_artefact_translations_sjw_artefacts_id = %s
+                """, (artefact_id,))
+                translations = {lang: {'name': name} for lang, name in self.cursor.fetchall()}
+                # Secondary stats
+                self.cursor.execute("""
+                    SELECT sjw_artefact_secondary_stats_name
+                    FROM sjw_artefact_secondary_stats
+                    WHERE sjw_artefact_secondary_stats_sjw_artefacts_id = %s
+                """, (artefact_id,))
+                secondary_stats = [sec_row[0] for sec_row in self.cursor.fetchall()]
                 artefacts.append({
-                    'id': a_row[0],
-                    'number': a_row[1],
-                    'image': a_row[2],
-                    'main_stat': a_row[3]
+                    'id': artefact_id,
+                    'name': translations.get(language, {}).get('name', ''),
+                    'set': artefact_set,
+                    'image_name': artefact_image,
+                    'main_stat': artefact_main_stat,
+                    'secondary_stats': secondary_stats,
+                    'number': artefact_number,
+                    'translations': translations
                 })
             # Cores
             self.cursor.execute("""
@@ -40,13 +74,15 @@ class SJWEquipmentSetSql:
                     'id': c_row[0],
                     'name': c_row[1],
                     'number': c_row[2],
-                    'image': c_row[3],
+                    'image_name': c_row[3],
                     'main_stat': c_row[4],
                     'secondary_stat': c_row[5]
                 })
             sets.append({
                 'id': set_id,
                 'name': set_name,
+                'description': set_description,
+                'focus_stats': focus_stats,
                 'artefacts': artefacts,
                 'cores': cores
             })
