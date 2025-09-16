7b5490df6d7f3eb3233a9b1182be6a30a605daca
BlazeOne <yann.tsatsaris@gmail.com>
2025-09-16 13:04:59 +0200

    feat: ajouter la prise en charge de la langue dans les méthodes de récupération des bénédictions offensives et défensives

--- static/Controleurs/sql_entities/sjw/sjw_blessings_sql.py
+++ static/Controleurs/sql_entities/sjw/sjw_blessings_sql.py
@@ -2,33 +2,39 @@ class SJWBlessingsSql:
     def __init__(self, cursor):
         self.cursor = cursor
 
-    def get_offensive_blessings(self, sjw_id):
+    def get_offensive_blessings(self, sjw_id, language):
         self.cursor.execute("""
-            SELECT sjw_offensive_blessings_id, sjw_offensive_blessings_name, sjw_offensive_blessings_description, sjw_offensive_blessings_image
-            FROM sjw_offensive_blessings
-            WHERE sjw_offensive_blessings_sjw_id = %s
-        """, (sjw_id,))
+            SELECT b.sjw_offensive_blessings_id, t.sjw_offensive_blessing_translations_name, t.sjw_offensive_blessing_translations_description, b.sjw_offensive_blessings_image
+            FROM sjw_offensive_blessings b
+            LEFT JOIN sjw_offensive_blessing_translations t
+                ON t.sjw_offensive_blessing_translations_blessing_id = b.sjw_offensive_blessings_id
+                AND t.sjw_offensive_blessing_translations_language = %s
+            WHERE b.sjw_offensive_blessings_sjw_id = %s
+        """, (language, sjw_id))
         return [
             {
                 'id': row[0],
-                'name': row[1],
-                'description': row[2],
+                'name': row[1] if row[1] else '',
+                'description': row[2] if row[2] else '',
                 'image': row[3]
             }
             for row in self.cursor.fetchall()
         ]
 
-    def get_defensive_blessings(self, sjw_id):
+    def get_defensive_blessings(self, sjw_id, language):
         self.cursor.execute("""
-            SELECT sjw_defensive_blessings_id, sjw_defensive_blessings_name, sjw_defensive_blessings_description, sjw_defensive_blessings_image
-            FROM sjw_defensive_blessings
-            WHERE sjw_defensive_blessings_sjw_id = %s
-        """, (sjw_id,))
+            SELECT b.sjw_defensive_blessings_id, t.sjw_defensive_blessing_translations_name, t.sjw_defensive_blessing_translations_description, b.sjw_defensive_blessings_image
+            FROM sjw_defensive_blessings b
+            LEFT JOIN sjw_defensive_blessing_translations t
+                ON t.sjw_defensive_blessing_translations_blessing_id = b.sjw_defensive_blessings_id
+                AND t.sjw_defensive_blessing_translations_language = %s
+            WHERE b.sjw_defensive_blessings_sjw_id = %s
+        """, (language, sjw_id))
         return [
             {
                 'id': row[0],
-                'name': row[1],
-                'description': row[2],
+                'name': row[1] if row[1] else '',
+                'description': row[2] if row[2] else '',
                 'image': row[3]
             }
             for row in self.cursor.fetchall()
