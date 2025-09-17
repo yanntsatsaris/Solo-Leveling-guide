737a98a9200dbbbd84402a4cf565464da62e88f8
BlazeOne <yann.tsatsaris@gmail.com>
2025-09-17 13:52:01 +0200

    fix: ajouter le traitement des descriptions pour les évolutions de l'arme

--- routes/RouteSJW.py
+++ routes/RouteSJW.py
@@ -289,8 +289,11 @@ def SJW(app: Flask):
         # Trouver l'arme correspondant au nom donné
         base_path = f'images/{folder}'
         all_tags = []  # Ajoute ici la liste des tags si besoin
-        weapon = None
         character_folder = None
+        if 'evolutions' in weapon and isinstance(weapon['evolutions'], list):
+            for evolution in weapon['evolutions']:
+                evolution['description_raw'] = evolution['description']
+                evolution['description'] = process_description(evolution['description'], all_tags, base_path)
 
         # Renvoyer le template avec les données de l'arme
         return render_template('weapon_details.html', weapon=weapon)
